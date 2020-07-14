1c2e07619de65c72626af098fb2ae49d308cb9a9
Şafak ÇIRAĞ <safakcirag@gmail.com>
2020-07-14 17:28:23 +0300

    aws security groups

--- aws/wordpress/main.tf
+++ aws/wordpress/main.tf
@@ -163,4 +163,62 @@ resource "aws_subnet" "wp_rds3_subnet" {
   }
 }
 
+# ----- rds subnet group -----
+resource "aws_db_subnet_group" "wp_rds_subnet_group" {
+  name       = "wp_rds_subnet_group"
+  subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}", "${aws_subnet.wp_rds2_subnet.id}", "${aws_subnet.wp_rds3_subnet.id}",]
 
+  tags = {
+    Name = "wp_rds_sng"
+  }
+}
+
+# -----  Subnet Associations -----
+resource "aws_route_table_association" "wp_public1_assoc" {
+  subnet_id = "${aws_subnet.wp_public1_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_public2_assoc" {
+  subnet_id = "${aws_subnet.wp_public2_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private1_assoc" {
+  subnet_id = "${aws_subnet.wp_private1_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private2_assoc" {
+  subnet_id = "${aws_subnet.wp_private2_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+# ----- security groups -----
+resource "aws_security_group" "wp_dev_sg" {
+  name = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # SSH
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.local_ip}"]
+  }
+
+  # HTTP
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.local_ip}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+}
